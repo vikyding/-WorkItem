c6481b3138df6af61f793d4798ef828bd20efbaa
vikyding <ding.men@husky.neu.edu>
2018-08-29 17:21:43 -0700

    Create RetryHandler First time

---
+++ Msgraph.WorkItem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Msgraph.WorkItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Msgraph.WorkItem/RetryHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using Microsoft.WindowsAzure;
+
+
+namespace Msgraph.WorkItem
+{
+    public class RetryHandler : DelegatingHandler
+    {
+        
+        // property RetryPolicy
+        //public RetryPolicy retryPolicy { get; private set; }
+
+        //private const static int maxRetry;
+
+
+        public RetryHandler()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RetryHandler(HttpMessageHandler innerHandler)
+        {
+            InnerHandler = innerHandler;
+        }
+
+
+        public override Task<HttpResponseMessage> SendAsyncy(HttpRequestMessage HttpRequestMessage, CancellationToken CancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendAsyncyHttpRequestMessage(HttpRequestMessage, CancellationToken CancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
