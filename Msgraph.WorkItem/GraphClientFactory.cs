1eb9e7fb2a72203bd9e54d4e58ce53abfad5869e
vikyding <ding.men@husky.neu.edu>
2018-09-19 17:07:31 -0700

    write draft for GraphClientFactory

--- Msgraph.WorkItem/GraphClientFactory.cs
+++ Msgraph.WorkItem/GraphClientFactory.cs
@@ -8,5 +8,40 @@ namespace Msgraph.WorkItem
 {
     class GraphClientFactory
     {
+
+        // default
     }
+
+    // GraphClientFactory implements IHttpClientFactory
+
+    // provide pre-configured 
+
+    // create Httt client with an array of delegation handlers as pipeline
+    // public HttpClient CreateClient(DelegatingHandler[] handlers){}
+
+    // create Http client with innermost handler
+    // public HttpClient Create(HttpMessageHandler innerHanddler, DelegationHandler[] handlers){}
+
+    // create pipeline
+    // public static HttpMessageHandler CreatePipeline(){}
+
+    // configure default Http handleing behavior
+        // Set default request timeout
+        // public TimeSpan OverallTimeout(){}
+
+        // Override defalut base address
+        // public SetBaseAddress(){}
+
+        // Set default headers
+        // public ? xxxHeader{set; get}
+
+        // Set default serilizer
+        // public ISerializer Serializer{get; set;}
+
+   
+    // configure http proxy 
+    // public ? SetHttpProxy(HttpHost proxy){}
+
+    // configure sovereign could 
+    // 
 }
